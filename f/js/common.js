47ff2e455089797074e94199216918e5c08e85ec
BerTepesh <Ber.Tepesh@gmail.com>
2015-08-20 17:52:51 +0300

    Карточка

--- f/js/common.js
+++ f/js/common.js
@@ -69,3 +69,20 @@ $(function(){
     });
   });
 })
+$(document).ready(function(){
+  $(".modal-inline").fancybox({
+    type: 'inline',
+    fixed:false,
+    maxWidth: "100%",
+    title: '',
+    padding: 0,
+    autoResize: false,
+    autoCenter: false,
+    fitToView: false,
+    helpers: {
+      overlay: {
+        fixed: false
+      }
+    }
+  }); 
+})
